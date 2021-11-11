30c826af63340a1f484ae10dbdc81bf10b768055
Matt Scilipoti <matt@scilipoti.name>
2021-11-10 21:23:40 -0500

    websites#show lists webpages & scan date

--- spec/views/websites/show.html.slim_spec.rb
+++ spec/views/websites/show.html.slim_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "websites/show", type: :view do
   before(:each) do
     @website = assign(:website, Website.create!(
       name: "TEST NAME",
-      url: "https://example.com"
+      url: "https://example.com",
+      webpages: [Webpage.create!(url: 'https://example.com')]
     ))
   end
 
@@ -16,5 +17,17 @@ RSpec.describe "websites/show", type: :view do
   it "renders attributes in <p>" do
     render
     expect(rendered).to have_css('p', text: /example.com/)
+  end
+
+  it "renders list of Webpages" do
+    render
+    expect(rendered).to have_css('h2', text: /Webpages/)
+    expect(rendered).to have_css('ol li.webpage', count: 1)
+  end
+
+  it "renders table of Scans" do
+    render
+    expect(rendered).to have_css('h2', text: /Webpage Scans/)
+    expect(rendered).to have_css('table.scans')
   end
 end
