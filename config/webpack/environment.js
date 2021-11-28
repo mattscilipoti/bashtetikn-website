49c1080da416dba92721fa310497bcde278e90ed
Matt Scilipoti <matt@scilipoti.name>
2021-11-27 20:26:04 -0500

    Install foundation 6
    See: https://railsbytes.com/public/templates/X6ksRW
    $ rails app:template LOCATION='https://railsbytes.com/script/X6ksRW'
    - upgrade code in config/webpack/environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.get('sass').use
+   .find(item => item.loader === 'sass-loader').options.sassOptions.includePaths.push(...['node_modules'])
+
 module.exports = environment
