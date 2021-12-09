9fc4606a05821649ec512f0ecab1c697592c2701
Matt Scilipoti <matt@scilipoti.name>
2021-12-08 23:15:00 -0500

    raisl 7: copy new rails 7 files
    - css to app/assets/stylesheets
    - no more webpacker, packs, or node_modules
    - no more spring
    - adds app/javascript/controllers
    - recreate credentials (not sure why credentials was failing)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,18 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import "@hotwired/turbo-rails"
+import "controllers"
+
 import "@fortawesome/fontawesome-free/css/all"
 
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
-require("css/application.scss")
 
 import { Foundation } from 'foundation-sites'
 import $ from 'jquery'
