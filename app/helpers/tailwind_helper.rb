1eff0ace034de6cb62810b42ef6b553f71921c90
Matt Scilipoti <matt@scilipoti.name>
2021-12-12 20:51:16 -0500

    Style: format menu

---
+++ app/helpers/tailwind_helper.rb
@@ -0,0 +1,10 @@
+module TailwindHelper
+  def tailwind_menu_list_item(caption, url_options, html_options={})
+    default_css_classes = ['mr-6']
+    default_css_classes << 'is-active' if current_page?(url_options)
+    default_html_options = { class: default_css_classes}
+    content_tag(:li, default_html_options) do
+      link_to caption, url_options, html_options
+    end
+  end
+end
