9bb44d663b5225a26f77d5d63bd095ac3256cb41
Matt Scilipoti <matt@scilipoti.name>
2021-12-13 21:31:05 -0500

    flash messages: add icon
    - add flash type :success
    - flash icons are aria-hidden

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  # standard element to display datetime
+  def datetime_el(element_container_type, datetime)
+    content_tag(element_container_type, title: datetime) do
+      datetime ? "#{time_ago_in_words(datetime)} ago" : nil
+    end
+  end
+
   # External links open in new tab and have 'new-window' icon
   # Unlike link_to, this requires link_text and url
   # NOTE: requires bootstrap for icon link portion
@@ -7,10 +14,23 @@ module ApplicationHelper
     link_to(link_text, url, default_html_options.merge(html_options))
   end
 
-  # standard element to display datetime
-  def datetime_el(element_container_type, datetime)
-    content_tag(element_container_type, title: datetime) do
-      datetime ? "#{time_ago_in_words(datetime)} ago" : nil
+  def flash_icon_name(flash_type)
+    case flash_type.to_sym
+    when :alert
+      'triangle-exclamation'
+    when :notice
+      'circle-info'
+    when :success
+      'square-check'
+    else
+      raise ArgumentError, "Unsupported flash_type (#{flash_type})"
+    end
+  end
+
+  def flash_tag(flash_type, message, caption: nil)
+    content_tag(:div, :class => "flash highlight #{flash_type}") do
+      icon = flash_icon_name(flash_type)
+      font_awesome_icon(icon, caption: caption, html_options: { aria: { hidden: true }}) + message
     end
   end
 end
--- app/helpers/font_awesome_helper.rb
+++ app/helpers/font_awesome_helper.rb
@@ -8,10 +8,13 @@ module FontAwesomeHelper
     uploaded: :fak
   }
 
-  def font_awesome_icon(image_name, caption: nil, icon_type: :solid)
+  def font_awesome_icon(icon_name, caption: nil, icon_type: :solid, html_options: {})
     image_type_abbreviation = IconTypeAbbreviations.fetch(icon_type)
-    image_classes = [image_type_abbreviation, "fa-#{image_name.to_s.parameterize}"]
+    image_classes = [image_type_abbreviation, "fa-#{icon_name.to_s.parameterize}"]
+    html_option_classes = html_options.fetch(:class, '').split(' ')
+    css_class = image_classes + html_option_classes
     caption = "&nbsp;#{caption}".html_safe if caption
-    content_tag(:i, caption, class: image_classes.join(' '))
+    html_options.merge!({class: css_class.join(' ')})
+    content_tag(:i, caption, html_options)
   end
 end
