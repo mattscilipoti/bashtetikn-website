f4c63f4030b24b51c5f11fd5d78bba2de86d603c
Matt Scilipoti <matt@scilipoti.name>
2021-12-14 01:16:35 -0500

    Menu: convert tailwind_menu_list_item to menu_list_item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,13 @@ module ApplicationHelper
       font_awesome_icon(icon, caption: caption) + message
     end
   end
+
+  def menu_list_item(caption, url_options, html_options={})
+    default_css_classes = []
+    default_css_classes << 'is-active' if current_page?(url_options)
+    default_html_options = { class: default_css_classes}
+    content_tag(:li, default_html_options) do
+      link_to caption, url_options, html_options
+    end
+  end
 end
